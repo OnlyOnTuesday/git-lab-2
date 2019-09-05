5dff5c04e1ab344bd0f819f97b60fed0533ef292
Michael Cooney <MC509119@ohio.edu>
2019-09-05 16:05:32 -0400

    Created git-lab-program.cc; prints Hello Gitgit status

---
+++ git-lab-program.cc
@@ -0,0 +1,22 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Cooney
+ *        Date: 09/05/19
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+
+int main(int argc, char const *argv[]) {
+  
+  /*add code*/
+
+  cout << "Hello Git!!" << endl;
+  
+  return 0;
+}// main
